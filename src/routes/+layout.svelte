1835685106256f661655bdaed8d28a76b11b66b9
Dev Rangie <devrangie25@gmail.com>
2024-03-04 00:54:39 +0800

    Fix Layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,6 @@
 	import type { PageData } from './$types';
     import 'tailwindcss/tailwind.css';
 	import '../app.css';
-    import "/node_modules/flag-icons/css/flag-icons.min.css";
 
 	export let data: PageData;
 </script>
